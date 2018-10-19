cf95ad2c1cd1cd9479bb7f446df65f67e9666da0
Mustafa <mustafamfaisal@gmail.com>
2018-10-19 23:20:22 +0300

    adding comments

--- fetchApi.js
+++ fetchApi.js
@@ -1,12 +1,14 @@
+// require axios module
 var axios = require('axios');
-
+// get data from the API
 axios.get('https://jsonplaceholder.typicode.com/posts')
 .then((result)=>{
     var response = "";
-    var userId = result.data.results[0].userId;
-    var title = result.data.results[0].title;
-    var body = result.data.results[0].body;
-    response= `${userId} coordinates: ${title}, ${body}`;
+    var userId = result.data.results[userId].userId;
+    var title = result.data.results[userId].title;
+    var body = result.data.results[userId].body;
+    response= `${userId} post title: ${title},: ${body}`;
     console.log(response);
 })
+// log the error number
 .catch((error)=>{console.log(error);});
