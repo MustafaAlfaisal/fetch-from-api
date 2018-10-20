fdc731ebfe3fca83d475afd26f6fb20496715010
Mustafa <mustafamfaisal@gmail.com>
2018-10-20 14:19:39 +0300

    adding comments

--- fetchApi.js
+++ fetchApi.js
@@ -3,11 +3,14 @@ var axios = require('axios');
 // get data from the API
 axios.get('https://jsonplaceholder.typicode.com/posts')
 .then((result)=>{
+    // decalre variables
     var response = "";
     var userId = result.data.results[userId].userId;
     var title = result.data.results[userId].title;
     var body = result.data.results[userId].body;
+    // store response
     response= `${userId} post title: ${title},: ${body}`;
+    // log the response
     console.log(response);
 })
 // log the error number
